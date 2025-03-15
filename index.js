5882903637a4a012a81b25999e3b777558d4523a
Rakib_Hossain_Novel <rakib.hossain@streamstech.com>
2025-03-16 01:39:06 +0600

    Added /users endpoint

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
 res.status(200).send(`Hello, from Node App on PORT: ${port}!`);
 });
 
+app.get('/users', (req, res) => {
+    res.status(200).send(`Congratulations! You have reached the users endpoint!`);
+});
 
 app.listen(port, () => {
 console.log(`App running on http://localhost:${port}`);
